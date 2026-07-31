dc3ecf138731ed40c017b6faebebc25ce2b02dc7
chereddy prasanthi <santhiram635@gmail.com>
2026-07-31 15:10:32 +0530

    terraform

---
+++ 40-databases/main.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "mongodb" {
+    ami = local.ami_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.mongodb_sg_id]
+    subnet_id = local.database_subnet_id
+    
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-mongodb" # roboshop-dev-mongodb
+        }
+    )
+}
+
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mongodb.private_ip
+  }
+
+#   # terraform copies this file to mongodb server
+#   provisioner "file" {
+#     source = "bootstrap.sh"
+#     destination = "/tmp/bootstrap.sh"
+#   }
+
+  provisioner "remote-exec" {
+    inline = [
+        "echo hello world"
+    ]
+  }
+}
