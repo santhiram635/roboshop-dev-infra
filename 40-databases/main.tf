037ecde156fe22b348d80aef0f9a34b3a595352c
chereddy prasanthi <santhiram635@gmail.com>
2026-08-01 10:52:46 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -24,15 +24,17 @@ resource "terraform_data" "mongodb" {
     host     = aws_instance.mongodb.private_ip
   }
 
-#   # terraform copies this file to mongodb server
-#   provisioner "file" {
-#     source = "bootstrap.sh"
-#     destination = "/tmp/bootstrap.sh"
-#   }
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
 
   provisioner "remote-exec" {
     inline = [
-        "echo hello world"
+        "chmod +x /tmp/bootstrap.sh",
+        # "sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh"
     ]
   }
 }
