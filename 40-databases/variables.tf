51eb598d4ecf9c26dab20ab891e391338dc4e490
chereddy prasanthi <santhiram635@gmail.com>
2026-08-04 12:50:20 +0530

    terraform

--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -5,3 +5,28 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "sg_names" {
+    default = [
+        # databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        # frontend
+        "frontend",
+        # bastion
+        "bastion",
+        # frontend load balancer
+        "frontend_alb",
+        # Backend ALB
+        "backend_alb"
+    ]
+}
+
+variable "zone_id" {
+    default = "Z006995023Z19U21ADZFP"
+}
+
+variable "domain_name" {
+    default = "mokshi.fun"
+}
