77158e1b5950718a9e9ee618ebbf90e055c62ae8
chereddy prasanthi <santhiram635@gmail.com>
2026-08-04 12:41:58 +0530

    terraform

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z006995023Z19U21ADZFP"
+}
+
+variable "domain_name" {
+    default = "mokshi.fun"
+}
