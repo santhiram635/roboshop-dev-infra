e7524e4079f0220aa23e0c05284736d10d7ea2f7
chereddy prasanthi <santhiram635@gmail.com>
2026-08-04 13:11:54 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -34,7 +34,7 @@ resource "terraform_data" "mongodb" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
         # "sudo sh /tmp/bootstrap.sh"
-        "sudo sh /tmp/bootstrap.sh mongodb"
+        "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
     ]
   }
 }
@@ -74,7 +74,7 @@ resource "terraform_data" "redis" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh redis"
+        "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
     ]
   }
 }
@@ -115,7 +115,7 @@ resource "terraform_data" "rabbitmq" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh rabbitmq"
+        "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}"
     ]
   }
 }
