2679ad20ba2e59250fcc04d89c3d51326ed173a6
chereddy prasanthi <santhiram635@gmail.com>
2026-08-05 20:52:33 +0530

    terraform

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z006995023Z19U21ADZFP"
+}
+
+variable "domain_name" {
+    default = "mokshi.fun"
+}
